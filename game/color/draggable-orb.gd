be911556ce8342c1d6de0c57be6344ba3ca2766b
zaftnotameni <mozair.carmo+zaft@gmail.com>
2024-08-04 07:44:47 +0200

    better color on the backsplash of liquids

--- game/color/draggable-orb.gd
+++ game/color/draggable-orb.gd
@@ -27,4 +27,5 @@ func update_color_based_on_element():
 	get_node('Label').text = FireworkData.Element.find_key(element)
 	material.set_shader_parameter('fill_value', 0.5)
 	material.set_shader_parameter('frontFillInnerColour', ColorMixingLogic.mix_elements([element]))
+	material.set_shader_parameter('backFillColour', ColorMixingLogic.mix_elements([element]).lightened(0.2))
 
--- game/color/droppable-orb.gd
+++ game/color/droppable-orb.gd
@@ -4,7 +4,13 @@ class_name DroppableOrb extends ColorRect
 @export var elements : Array[FireworkData.Element]
 
 func _enter_tree() -> void:
-	if not elements: elements = [FireworkData.Element.Mg]
+	if not elements: elements = []
+	update_color_based_on_elements()
+
+func _notification(what: int) -> void: ToolScriptHelpers.on_pre_save(what, on_pre_save)
+
+func on_pre_save():
+	if not ToolScriptHelpers.is_valid_tool_target(self): return
 	update_color_based_on_elements()
 
 func _can_drop_data(_at_position: Vector2, data: Variant) -> bool:
@@ -17,5 +23,6 @@ func _drop_data(_at_position: Vector2, data: Variant) -> void:
 
 func update_color_based_on_elements():
 	material.set_shader_parameter('frontFillInnerColour', ColorMixingLogic.mix_elements(elements))
+	material.set_shader_parameter('backFillColour', ColorMixingLogic.mix_elements(elements).lightened(0.2))
 	material.set_shader_parameter('fill_value', -1 + (elements.size() * 0.10))
 
