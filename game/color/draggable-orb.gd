f1f103fb85ccd09110e78cdb74e93d0e9aedbf66
zaftnotameni <mozair.carmo+zaft@gmail.com>
2024-08-04 07:33:28 +0200

    fireworks color working scene with names

--- game/color/draggable-orb.gd
+++ game/color/draggable-orb.gd
@@ -6,6 +6,15 @@ class_name DraggableOrb extends ColorRect
 func _enter_tree() -> void:
 	update_color_based_on_element()
 
+func _notification(what: int) -> void: ToolScriptHelpers.on_pre_save(what, on_pre_save)
+
+func _ready() -> void:
+	update_color_based_on_element()
+
+func on_pre_save():
+	if not ToolScriptHelpers.is_valid_tool_target(self): return
+	update_color_based_on_element()
+
 func _get_drag_data(_at_position: Vector2) -> Variant:
 	var ghost := duplicate()
 	ghost.custom_minimum_size = Vector2(32, 32)
@@ -15,5 +24,7 @@ func _get_drag_data(_at_position: Vector2) -> Variant:
 	return element
 
 func update_color_based_on_element():
+	get_node('Label').text = FireworkData.Element.find_key(element)
+	material.set_shader_parameter('fill_value', 0.5)
 	material.set_shader_parameter('frontFillInnerColour', ColorMixingLogic.mix_elements([element]))
 
